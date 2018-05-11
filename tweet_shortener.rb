3730c2a1e250427b700aa57ea64661f444ec7748
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-11 20:00:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,5 +30,10 @@ def bulk_tweet_shortener(tweets)
   puts word_substituter(tweets)
 end 
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
+  if tweet.length < 140 
+    tweet 
+  else 
+    word_substituter(tweet)
+  end 
 end 
