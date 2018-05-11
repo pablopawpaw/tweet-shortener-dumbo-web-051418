e998cbe0ffdf1303e4bf10c2380ed3837f1b735b
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-11 19:44:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary 
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"  
+  }
+end 
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  
+  tweet_array.map do |word|
+    if dictionary.keys.include?(word)
+      word = dictionary[word]
+    else 
+      word
+    end 
+  end.join(" ") 
+end 
+
+def bulk_tweet_shortener
+end 
+
+def selective_tweet_shortener
+end 
